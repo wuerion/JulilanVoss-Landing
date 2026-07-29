0f63b2a52da6b869ccceeafda2cce79cf6274ef5
emmanuel <almerux@gmail.com>
2026-07-28 19:55:10 -0600

    chore(sitemap): add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  site: 'https://julianvoss.axvolestudio.com',
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), sitemap()],
 });
